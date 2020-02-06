f3abfa2ea8d5690ded8610b17ff44db4ecac942f
Carlos-Ignacio-Alvarez-Picazo <60673590+Carlos-Ignacio-Alvarez-Picazo@users.noreply.github.com>
2020-02-06 16:35:46 -0600

    Programa para generar tres numeros aleatorios

---
+++ tres_numeros_aleatorios.js
@@ -0,0 +1,24 @@
+/*generar tres numeros aleatorios rango de 0 a 19
+mostrar el menor*/
+var a=Math.floor(math,random()*20);
+var b=math.floor(math.random()*20);
+var c=math.floor(math.random()*20);
+ console.log(a);
+ console.log(b);
+ console.log(c);
+
+ if (a<b)
+     if (a<c)
+ console.log("el numero nemor es: ",a);
+         else
+ console.log("El numero menor es:",c);
+                else
+                     if(b<c)
+console.log("el numero menor es: ",b);
+                            else
+ c
+                                if (a<b && a<c)
+ a
+                                       else
+                                          if (b<a && b<c)
+ c
